e3079284a7929b0919bf92a139fb396403f81abc
mocmoc15 <aiai2.0831@gmail.com>
2021-12-12 23:51:31 +0900

    トップページ表示、バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname, presence: true 
   validates :birthday, presence: true
 
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "is invalid. Include both letters and numbers" }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters" } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" } do
+    validates :last_name_kana 
+    validates :first_name_kana
+  end
 
 end
