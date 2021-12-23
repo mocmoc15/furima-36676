6a7b2118630b994326e0afdb63e7e80c439d5885
mocmoc15 <aiai2.0831@gmail.com>
2021-12-23 17:02:08 +0900

    商品情報編集機能（修正１回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
 
 def index
@@ -31,7 +31,6 @@ def edit
 end
 
 def update
-  @item = Item.find(params[:id])
   if @item.update(item_params)
     redirect_to item_path
   else
