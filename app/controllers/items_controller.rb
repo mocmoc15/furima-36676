7dbc35889846657fc859c848b68bb4c9b98b5661
mocmoc15 <aiai2.0831@gmail.com>
2021-12-24 00:07:47 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
 def index
@@ -36,6 +36,11 @@ def update
   else
     render :edit
   end
+end
+
+def destroy
+  @item.destroy
+  redirect_to root_path
 end
 
 private
