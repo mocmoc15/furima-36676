b961223a55c959ce130ceee8b5f7983ba25a4380
mocmoc15 <aiai2.0831@gmail.com>
2021-12-27 00:44:10 +0900

    配送先情報保存のテストコード

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_address do
+    user_id            { 1 }
+    item_id            { 1 }
+    postal_code        { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    prefecture_id      { Faker::Number.between(from: 2, to: 48) }
+    municipality       { Faker::Address.city }
+    address            { Faker::Address.street_address }
+    building           { Faker::Address.street_address }
+    telephone_number   { Faker::Number.decimal_part(digits: 11) }
+  end
+end
