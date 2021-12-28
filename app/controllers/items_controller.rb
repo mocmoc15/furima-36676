efb1137186f9a69c49d66815956220315ae63711
mocmoc15 <aiai2.0831@gmail.com>
2021-12-28 16:08:53 +0900

    商品購入機能（修正４回目）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ def show
 end
 
 def edit
-  if current_user.id == @item.user_id && @item.order.present?
+  if current_user.id != @item.user_id || @item.order.present?
     redirect_to root_path
   end
 end
