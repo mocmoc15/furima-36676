a695bc74c6c21f0aeeefc3178e90c03c3fba2e03
mocmoc15 <aiai2.0831@gmail.com>
2022-01-06 05:14:38 +0900

    出品機能バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -26,10 +26,10 @@ class Item < ApplicationRecord
     validates :time_to_ship_id
   end
 
-  with_options presence: true do
-    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 ,message: "is out of setting range"}
-  end
+
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 ,message: "is out of setting range"}
 
 
 end
